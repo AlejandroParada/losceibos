f46ff9285a72b0f2cff1957a9594927bb807337e
Alejandro Parada <alejandroparada@hospitalevangelico.com>
2020-02-07 13:18:54 -0300

    Otros ajustes

--- deploy.cmd
+++ deploy.cmd
@@ -2,19 +2,9 @@
 git add .
 git commit -m "deploy"
 git push
-rem git checkout master
+git checkout master
 
 git push origin --delete gh-pages
-rem git branch gh-pages
-
-npm run build
-
 git branch gh-pages
-git subtree split --prefix dist -b gh-pages 
 
-rem create a local gh-pages branch containing the splitted output folder
-
-git push -f origin gh-pages:gh-pages 
-
-rem force the push of the gh-pages branch to the remote gh-pages branch at origin
-git branch -D gh-pages 
+npm run deploy
