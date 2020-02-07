96a9650b724cfde2ff2be09ca81f12a6d5aaa3a2
Alejandro Parada <alejandroparada@hospitalevangelico.com>
2020-02-07 13:05:21 -0300

    deploy

---
+++ pags/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    dest: 'dist',
+    base: '/losceibos/',
+    title: "Rama Los Ceibos",
+    description: 'Unidad de la Estaca Uruguay Montevideo Maroñas',
+    themeConfig: {
+        sidebar: [
+          'Organizacion',
+          'Mensajes',
+          'Historia',
+          'Actividades'          
+        ]
+    }
+  }
